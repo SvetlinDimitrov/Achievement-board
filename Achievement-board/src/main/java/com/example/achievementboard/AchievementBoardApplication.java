dd301d01e718db61bba29df27095c8dd2adc7ecb
Svetlin DImitrov <prolama6@gmail.com>
2023-07-18 14:48:20 +0300

    add tamplates , resurses

---
+++ Achievement-board/src/main/java/com/example/achievementboard/AchievementBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.achievementboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchievementBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AchievementBoardApplication.class, args);
+    }
+
+}
---
+++ Achievement-board/src/test/java/com/example/achievementboard/AchievementBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.achievementboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchievementBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
